6fd701191b4f5c146fe5619109c2ec203e2828ca
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2018-05-20 15:55:28 -0400

    Signup flow laid out. No functionality yet

---
+++ UNCC-YesPlus/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  UNCC-YesPlus
+//
+//  Created by Akshay Ayyanchira on 5/20/18.
+//  Copyright © 2018 Akshay Ayyanchira. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
