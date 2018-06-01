e07ddb90a468b9e96dc58d351722befdb024d95d
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2018-05-31 23:43:31 -0400

    Login and Logout feature implemented

--- UNCC-YesPlus/ViewControllers/HomeViewController.swift
+++ UNCC-YesPlus/ViewControllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -19,35 +20,28 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         let yesplusImageView = UIImageView(image: #imageLiteral(resourceName: "YesPlusImage"), highlightedImage: #imageLiteral(resourceName: "YesPlusImage"))
         yesplusImageView.contentMode = .scaleAspectFit
         self.navigationItem.titleView = yesplusImageView
-        
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     // MARK : Table view implementation
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
+    
+    //PRAGMA MARK: Table view delegate methods
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customHomeCell") as! CustomHomeTableViewCell
         cell.thumbnail.image = #imageLiteral(resourceName: "Event")
         return cell
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutButtonPressed(_ sender: UIBarButtonItem) {
+        UserDefaults.standard.removeObject(forKey: "uuid")
+        do {
+            try Auth.auth().signOut()
+        } catch let signoutError as NSError {
+            print(signoutError.localizedDescription)
+        }
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
--- UNCC-YesPlus/ViewControllers/LoginViewController.swift
+++ UNCC-YesPlus/ViewControllers/LoginViewController.swift
@@ -12,6 +12,10 @@ import GoogleSignIn
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
 
+    
+    @IBOutlet weak var emailTextField: UsernameTextField!
+    @IBOutlet weak var passwordTextField: UsernameTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +29,27 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func loginButtonPressed(_ sender: LoginAndSignUpButton) {
+        if emailTextField.text != "" && passwordTextField.text != ""{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+                if error != nil{
+                    let alertController = UIAlertController(title: "Log in failed", message: "Please enter valid email id and password. Or click on Create New Account to get started", preferredStyle: UIAlertControllerStyle.alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(okAction)
+                    self.show(alertController, sender: nil)
+                }
+                else{
+                    let uuid = user?.uid
+                    UserDefaults.standard.set(uuid, forKey: "uuid")
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "loginSuccessfull", sender: nil)
+                    }
+                }
+            })
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
